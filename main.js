b20165eb1daa55698e76a143993547ca11b27ffc
banjman81 <benjamin.man97.com>
2021-06-16 13:20:54 -0500

    9/9 pass

--- main.js
+++ main.js
@@ -1,6 +1,71 @@
 // ┌─────────────────┐
 // │ Your code here! │
 // └─────────────────┘
+function createUser(a,b){
+    const user ={
+        firstName: a,
+        lastName: b
+    }
+    return user
+}
+
+function setAge(user, n){
+    createUser(user)
+    user.age = n;
+    return user
+}
+
+function incrementAge(user){
+    user.age+=1
+    return user
+}
+
+function fixCar(a){
+    a.needsMaitenance = false;
+    return a
+}
+
+
+function addGrades(a, arr){
+    for (const ar of arr){
+        a.grades.push(ar)
+    }
+    return a
+}
+function getDataType(a, b){
+    const result = typeof a[b]
+    return result
+}
+
+function addTodo(a, b){
+    a.push(b)
+    return a
+}
+
+function addSong(a, b){
+    a.songs.push(b)
+    a.duration += b.duration
+    return a
+}
+
+function updateReportCard(a, b){
+    let sum = 0;
+    a.grades.push(b)
+    for (const g of a.grades){
+        sum += g
+        if(g < a.lowestGrade){
+            a.lowestGrade = g
+        }
+        else if(g> a.highestGrade){
+            a.highestGrade = g
+        }
+    }
+    a.averageGrade = sum/a.grades.length
+    return a
+}
+
+
+
 
 
 
